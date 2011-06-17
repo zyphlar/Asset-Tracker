9efaf8ab5aad3449ef33ef338271fedd279133ad
Jason Katz <jasonkatz88@gmail.com>
2011-06-17 11:05:35 -0700

    some small changes to get rails working hopefully

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
+  RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require 'thread'
@@ -22,7 +22,7 @@ Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
-  config.action_mailer.default_url_options = { :host => 'trilliumai.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'moonscoopai.heroku.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
 
@@ -34,9 +34,9 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem 'warden', :version => '0.10.7'
-  config.gem 'devise', :version => '1.0.9'
-  config.gem 'paginate'
+    config.gem 'warden', :version => '0.10.7'
+    config.gem 'devise', :version => '1.0.9'
+    config.gem 'paginate'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
